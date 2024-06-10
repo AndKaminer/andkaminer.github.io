072713750bacaa951063f7235d6f919c47f7e651
Andrew Kaminer <94922098+AndKaminer@users.noreply.github.com>
2024-06-09 20:39:00 -0400

    Initial commit

---
+++ footer.js
@@ -0,0 +1,16 @@
+document.write('\
+        <footer>\
+            <div>\
+                <a href="mailto:akaminer@gatech.edu" class ="fa fa-envelope"></a>\
+            </div>\
+            <div>\
+                <a href="https://linkedin.com/in/andrew-kaminer-a89156228" class="fa fa-linkedin" target="_blank"></a>\
+            </div>\
+            <div>\
+                <a href="https://github.com/AndKaminer" class="fa fa-github" target="_blank"></a>\
+            </div>\
+            <div>\
+                <a href="https://twitter.com/andrew_kaminer" class="fa fa-twitter" target="_blank"></a>\
+            </div>\
+        </footer>\
+');
---
+++ header.js
@@ -0,0 +1,8 @@
+document.write('\
+        <header>\
+            <a href="index.html">Home</a>\
+            <a href="documents/resume-summer-2024.pdf" target="_blank">Resume</a>\
+            <a href="training.html">Training Log</a>\
+            <a href="reading-list.html">Reading List</a>\
+        </header>\
+');
