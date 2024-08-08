3c262084db00f2304aa8963b2e152396e0afe526
Andrew Kaminer <94922098+AndKaminer@users.noreply.github.com>
2024-08-07 20:34:44 -0400

    Update header.js

--- js/header.js
+++ js/header.js
@@ -2,7 +2,6 @@ document.write('\
         <header>\
             <a href="index.html">Home</a>\
             <a href="documents/resume-summer-2024.pdf" target="_blank">Resume</a>\
-            <a href="training.html">Training Log</a>\
             <a href="reading-list.html">Reading List</a>\
         </header>\
 ');
