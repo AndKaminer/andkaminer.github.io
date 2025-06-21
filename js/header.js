fa65f7dbe4e8b31235bb0a1c51b62f77448cacb9
Andrew Kaminer <akaminer@gatech.edu>
2025-06-20 22:21:16 -0400

    Purge

--- js/header.js
+++
@@ -1,7 +0,0 @@
-document.write('\
-        <header>\
-            <a href="index.html">Home</a>\
-            <a href="documents/AndrewKaminerResume.pdf" target="_blank">Resume</a>\
-            <a href="reading-list.html">Reading List</a>\
-        </header>\
-');
